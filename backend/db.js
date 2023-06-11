f0b076c7c35ecee09a40bdd0f042c50b34502934
satyam18082003 <127774286+satyam18082003@users.noreply.github.com>
2023-06-11 13:54:33 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://Satyam:Satyam@ac-botywsm-shard-00-00.puhf6nh.mongodb.net:27017,ac-botywsm-shard-00-01.puhf6nh.mongodb.net:27017,ac-botywsm-shard-00-02.puhf6nh.mongodb.net:27017/QandA?ssl=true&replicaSet=atlas-thswq8-shard-0&authSource=admin&retryWrites=true&w=majority"
-
-  // "mongodb://akky:KdaWNLd6wxADLvy@cluster0-shard-00-00.rfatk.mongodb.net:27017,cluster0-shard-00-01.rfatk.mongodb.net:27017,cluster0-shard-00-02.rfatk.mongodb.net:27017/stackoverflow?ssl=true&replicaSet=atlas-i16i1b-shard-0&authSource=admin&retryWrites=true&w=majority";
+const url = "" //Enter Yours Mongo DB Url
 module.exports.connect = () => {
   mongoose
     .connect(url, {
